1c14f80e6b959b8db53d3325e0b23ad0f7c5f2a4
EddieHooks36 <eddiehooks36@gmail.com>
2022-12-08 18:28:05 -0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Script file");
-var age=100;//Initialization and Assignation
+var age=100;//Initialization and Assignationl (old variable types)
 let userName="Mike";
 let taxes = 11.5;
 const URL = "http://www/thisisprivateEndPoint/project";
@@ -13,7 +13,7 @@ console.log(userName,URL);//displaying the userName and URL
 
 let numberOfChildren = 3;
 let partnerName = "Jennifer";
-let geoLocation = "Los Angeles";
+let geoLocation = "Laguna Hills";
 let jobTitle = "IT Specialist";
 
 console.log("I have" + numberOfChildren, "Married to" + partnerName,"I live" + geoLocation,"I am an IT Specialist" + jobTitle)
@@ -22,4 +22,5 @@ console.log("I have" + numberOfChildren, "Married to" + partnerName,"I live" + g
 document.write("<p>You will be a" + jobTitle + "in" + geoLocation + "and married to" + partnerName + "with" + numberOfChildren + "kids</p>");
 
 //template string
-document.write(`<p> You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numberOfChildren + 1} kids</p>`);
+document.write(`<p> You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numberOfChildren} kids</p>`);
+
---
+++ script2.js
@@ -0,0 +1,29 @@
+
+//let = userName = "Edward"; prompt("Enter your name");//JS function
+//let = userEmail = "eddie@gmail.com"; prompt("Enter your email
+//let = userPassword = "seven";
+//let = userAge = "33"; 
+//let = userCountry = "US";
+//let = monthlySalary = "$6200/month";
+
+//console.log(userName,userEmail,monthlySalary);
+
+//document.write(`
+     //<p>user: ${userName}</p>
+    //<p>Email: ${userEmail}</p>
+    //<p>Salary: ${monthlySalary}</p>
+    //`)
+   
+//console.log(userName,userEmail,monthlySalary);
+
+//creating functions
+
+//function declaration
+function greeting(){
+    //body of the function
+    console.log("Hello");
+    alert("Hello");
+    document.write("Hello")
+}
+
+//function expression
